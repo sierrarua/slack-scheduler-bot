ff1f007fe29c7eec390f94f35249ad65652d0686
git status <ychen10@wellesley.edu>
2018-07-24 13:53:22 -0700

    tuesday

--- env.sh
+++ env.sh
@@ -4,3 +4,6 @@ export REDIRECT_URL='http://localhost:1337'
 export SLACK_BOT_TOKEN='xoxb-402883401617-403207694372-DJNtg4Aw90cPgQjIDdZ6iQMH'
 export AUTH_TOKEN='xoxp-402883401617-402883402721-402736016128-c9693631bff0c6f7f0b9ec863b98169f'
 export GOOGLE_APPLICATION_CREDENTIALS='./scheduler-bot-20faa067c5cb.json'
+export MONGODB_URI='
+mongodb://ychen10:Cuijiaming2019@ds147461.mlab.com:47461/scheduler-bot-horizons
+'
