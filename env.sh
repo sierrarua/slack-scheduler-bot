1f32a7fe7ce5ff636101be09b732c75d8680ffa8
Sierra <jiang26s@mtholyoke.edu>
2018-07-25 09:18:16 -0700

    update

---
+++ env.sh
@@ -0,0 +1,7 @@
+export CLIENT_ID="1090849197586-vvkes74pft984uc96ogkjd9faej26ki9.apps.googleusercontent.com
+export"
+export CLIENT_SECRET="1090849197586-vvkes74pft984uc96ogkjd9faej26ki9.apps.googleusercontent.com
+"
+export REDIRECT_URL="http://localhost:1337/oauthcallback"
+export SLACK_BOT_TOKEN="xoxb-402883401617-403207694372-DJNtg4Aw90cPgQjIDdZ6iQMH"
+export 
